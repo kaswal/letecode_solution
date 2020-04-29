a58eeec1d48c3a8ba41a4ae077031a7df1885e97
kautilya aswal <er.kautilya21@gmail.com>
2020-04-29 15:48:55 -0700

    solution

---
+++ Medium/word break.py
@@ -0,0 +1,17 @@
+def word_break(s, wd):
+    dp = [False] * (len(s) + 1)
+
+    dp[0] = True
+
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            if dp[i] and s[i:j+1] in wd:
+                dp[j+1] = True
+
+    return dp[-1]
+
+
+mystr = 'applepenapple'
+wordDict = ["apple", "pen"]
+
+print(word_break(mystr, wordDict))
