c2572ec103fd6967c8e814043371c161965fe0b6
kautilya aswal <er.kautilya21@gmail.com>
2020-05-04 12:06:15 -0700

    update

--- Medium/longest increasing subsequence.py
+++ Medium/longest increasing subsequence.py
@@ -6,7 +6,7 @@ def longest_increasing_subseq(arr):
             if arr[j] < arr[i]:
                 dp[i] = max(dp[i], dp[j] + 1)
 
-    return dp
+    return max(dp)
 
 
 print(longest_increasing_subseq([3, 4, -1, 0, 6, 2, 3]))
