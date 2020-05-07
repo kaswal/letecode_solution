439da6e5ad3c81ae41d3be1d9933a9ad10c3af1d
kautilya aswal <er.kautilya21@gmail.com>
2020-05-06 18:53:39 -0700

    solution

--- Medium/test.py
+++ Medium/test.py
@@ -1,13 +1,24 @@
-mydict = {1: 3, 2: 2, 3: 1}
-# mydict = {1: 1}
-k = 2
-i = 0
-arr = []
-while i < k:
+arr = [3, 4, 2, 1]
+dp = []
+rows = 4
+cols = 11
 
-    arr.append(mydict.get(max(mydict.values())))
-    del mydict[mydict.get(max(mydict.values()))]
+for i in range(rows):
+    col = []
+    for j in range(cols):
+        if j == 0:
+            col.append(True)
+        else:
+            col.append(False)
+    dp.append(col)
 
-    k -= 1
+dp[0][arr[0]] = True
 
-print(arr)
+for i in range(1, rows):
+    for j in range(1, cols):
+        if arr[i] <= j and dp[i - 1][j] == False:
+            dp[i][j] = dp[i - 1][j - arr[i]]
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp)
